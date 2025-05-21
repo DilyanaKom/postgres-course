ea6214d7f6e5963f98645575a2d074a8a4eb0a9f
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-05-21 20:37:34 +0300

    Create 20-multiple-tables-view.sql

---
+++ 01-basic-crud/solutions/20-multiple-tables-view.sql
@@ -0,0 +1,10 @@
+SELECT
+	concat(e.first_name, ' ', e.last_name) AS full_name,
+	e.department_id,
+	concat(a.number, ' ', a.street) AS address
+FROM 
+	employees AS e
+JOIN 
+	addresses AS a
+ON 
+	a.id = e.address_id
